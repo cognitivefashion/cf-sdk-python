5072573f3a3f35a03297ee7214cb5ce20a148fa7
Vikas Raykar <viraykar@in.ibm.com>
2018-02-05 20:53:31 +0530

    Adding person detection API

---
+++ person.py
@@ -0,0 +1,59 @@
+#------------------------------------------------------------------------------
+# Person detection.
+# POST /v1/person
+#------------------------------------------------------------------------------
+
+import os
+import json
+import requests
+from urlparse import urljoin
+from pprint import pprint
+
+from props import *
+
+# Replace this with the custom url generated for you.
+api_gateway_url = props['api_gateway_url']
+
+# Pass the api key into the header
+# Replace 'your_api_key' with your API key.
+headers = {'X-Api-Key': props['X-Api-Key']}
+
+params = {}
+
+# Optional parameters.
+params['model'] = 'fast' # default
+#params['model'] = 'accurate'
+
+api_endpoint = '/v1/person'
+
+url = urljoin(api_gateway_url,api_endpoint)
+
+# Three options to pass the image
+
+# OPTION 1 : Directly post the image
+headers['Content-Type'] = 'image/jpeg'
+response = requests.post(url,
+                         headers=headers,
+                         params=params,
+                         data=open('test_image_4.jpeg','rb'))
+
+"""
+# OPTION 2 : Pass the image url
+params['image_url'] = 'http://vg-images.condecdn.net/image/oDXPOxw65EZ/crop/405'
+response = requests.post(url,
+                         headers=headers,
+                         params=params)
+"""
+
+"""
+# OPTION 3 : using multipart
+image_filename = 'test_image_3.jpeg'
+with open(image_filename,'rb') as images_file:
+    response = requests.post(url,
+                             headers=headers,
+                             params=params,
+                             files={'image': (image_filename,images_file,'image/jpeg')})   
+"""
+
+print response.status_code
+pprint(response.json())
